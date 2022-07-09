bad2fa8a19a274591f1d26cf183bb10c2793d11a
ferneynava <ferneynava@gmail.com>
2022-07-08 21:29:05 -0500

    :white_check_mark: feat: Agrego codigo pulsador y motores
    feat: Agrego codigo pulsador y motores

--- Robot-Sumo.ino
+++ Robot-Sumo.ino
@@ -1,7 +1,57 @@
-void setup(){
+#include <AFMotor.h>
+//Pines arduino a utilizar 
+AF_DCMotor MotorIz(1);
+AF_DCMotor MotorDer(2); 
+#define boton 2
+
+int VelocidadAtaque = 255;
+int VelocidadBusqueda = 200;
+ 
+void setup() { 
+  // put your setup code here, to run once:
+    pinMode(boton,INPUT);
+    MotorIz.setSpeed(VelocidadAtaque);
+    MotorDer.setSpeed(VelocidadAtaque);
+
+    Detener(); 
+
+    //Bloque para indicar que fue presionado el boton inicio de la competencia 5 segundos de seguridad
+    while (true)
+    {
+        int pulsado = digitalRead(boton);
+        if(pulsado == HIGH){
+            delay(5000); //5 segundos de seguridad
+            break;
+        }
+    } 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
 
 }
 
-void loop(){
-  
+void Detener(){
+    MotorIz.run(RELEASE);
+    MotorDer.run(RELEASE); 
+}
+
+void Adelante (){
+    MotorIz.run(FORWARD);
+    MotorDer.run(FORWARD);
+}
+
+void Atras(){
+    MotorIz.run(BACKWARD);
+    MotorDer.run(BACKWARD);
+}
+
+void GiroIzquierda(){
+    MotorIz.run(BACKWARD);
+    MotorDer.run(FORWARD);
+}
+
+void GiroDerecha(){
+    MotorIz.run(FORWARD);
+    MotorDer.run(BACKWARD);
 }
